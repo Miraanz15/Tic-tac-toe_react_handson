6eb4536eb17983cbfd51a11914aa3868b7483f27
Miraanz15 <miraanejaz15@gmail.com>
2023-06-07 04:02:11 +0530

    Creating Board

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
 import React from 'react';
-export default function Square() {
-  return <button className="square">X</button>;
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+export default function Board() {
+  return (
+  <div>
+    <div>
+       <button className="board-row">1</button>
+       <button className="board-row">2</button>
+       <button className="board-row">3</button>
+    </div>
+    <div>
+       <button className="board-row">4</button>
+       <button className="board-row">5</button>
+       <button className="board-row">6</button>
+    </div>
+    <div>
+       <button className="board-row">7</button>
+       <button className="board-row">8</button>
+       <button className="board-row">9</button>
+    </div>
+  </div>  
+  
+  );
 }
