254518a544aea5435df0fd1e06e38d4346283380
Miraanz15 <miraanejaz15@gmail.com>
2023-06-07 04:13:38 +0530

    More concept added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+function Square({value}){
+  return <button className="square">{value}</button>
+}
 export default function Board() {
   return (
   <div>
-    <div>
-       <button className="board-row">1</button>
-       <button className="board-row">2</button>
-       <button className="board-row">3</button>
+    <div className="board-row">
+       <Square value = "1"/>
+       <Square value = "2"/>
+       <Square value = "3"/>
     </div>
-    <div>
-       <button className="board-row">4</button>
-       <button className="board-row">5</button>
-       <button className="board-row">6</button>
+    <div className="board-row">
+       <Square value = "4"/>
+       <Square value = "5"/>
+       <Square value = "6"/>
     </div>
-    <div>
-       <button className="board-row">7</button>
-       <button className="board-row">8</button>
-       <button className="board-row">9</button>
+    <div className="board-row">
+       <Square value = "7"/>
+       <Square value = "8"/>
+       <Square value = "9"/>
     </div>
   </div>  
   
